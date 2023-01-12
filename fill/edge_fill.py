2c923d1c331e6408c8dd4dd633b9e4f2b7512524
Salies <serezane@protonmail.com>
2023-01-12 13:45:02 -0300

    edge-fill: por polígono

--- fill/edge_fill.py
+++ fill/edge_fill.py
@@ -1,18 +1,13 @@
 import numpy as np
 from PIL import Image, ImageDraw
 
-# Abrindo a imagem
-im = Image.open('img/edge.png')
+# Definindo o polígono
+
 
 # Definindo o bounding-box
 # x-min, y-min, x-max, y-max
 bb = [(14, 35), (184, 165)]
 
-# Draw a rectangle in the bounding box area
-im2 = Image.new('RGBA', (200, 200))
-img1 = ImageDraw.Draw(im2)
-img1.rectangle(bb, fill =(255, 0, 0))
+x_min, y_min = bb[0]
+x_max, y_max = bb[1]
 
-im3 = Image.blend(im, im2, 0.5)
-
-im3.show()
