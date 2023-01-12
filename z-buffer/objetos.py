b72186c4781d968c48939f2e43bb6792f21308a5
Salies <serezane@protonmail.com>
2023-01-12 00:29:08 -0300

    z-buffer: array de pontos

--- z-buffer/objetos.py
+++ z-buffer/objetos.py
@@ -20,16 +20,18 @@ sphere = lambda a, b: (
 
 zb = ZBuffer((300, 300), 150)
 
+points = []
+
 # Construindo os objetos
 # Calha azul
 for x in range(10, 31):
     for y in range(20, 41):
-        zb.set_point(x, y, curve(x, y), (0, 0, 255))
+        points.append((x, y, curve(x, y), (0, 0, 255)))
 
 # Plano vermelho
 for x in range(50, 101):
     for y in range(30, 81):
-        zb.set_point(x, y, plane(x, y), (255, 0, 0))
+        points.append((x, y, plane(x, y), (255, 0, 0)))
 
 # Cilindro amarelo
 cilinder_points = []
@@ -40,7 +42,7 @@ for a in np.linspace(0, 2 * np.pi, 1000):
         cilinder_points.append((x, y, z))
 cilinder_points = list(set(cilinder_points))
 for x, y, z in cilinder_points:
-    zb.set_point(x, y, z, (255, 255, 0))
+    points.append((x, y, z, (255, 255, 0)))
 
 # Esfera verde
 sphere_points = []
@@ -52,19 +54,21 @@ for a in np.linspace(0, 2 * np.pi, 500):
         sphere_points.append((x, y, z))
 sphere_points = list(set(sphere_points))
 for x, y, z in sphere_points:
-    zb.set_point(x, y, z, (0, 255, 0))
+    points.append((x, y, z, (0, 255, 0)))
 
 # Cubo branco
 for x in range(-20, 21):
     for y in range(-20, 21):
-        zb.set_point(x, y, -20, (255, 255, 255))
-        zb.set_point(x, y, 20, (255, 255, 255))
-        zb.set_point(x, 20, y, (255, 255, 255))
-        zb.set_point(x, -20, y, (255, 255, 255))
-        zb.set_point(20, x, y, (255, 255, 255))
-        zb.set_point(-20, x, y, (255, 255, 255))
+        points.append((x, y, -20, (255, 255, 255)))
+        points.append((x, y, -20, (255, 255, 255)))
+        points.append((x, y, 20, (255, 255, 255)))
+        points.append((x, 20, y, (255, 255, 255)))
+        points.append((x, -20, y, (255, 255, 255)))
+        points.append((20, x, y, (255, 255, 255)))
+        points.append((-20, x, y, (255, 255, 255)))
 
-# purple
-zb.set_point(10, 100, 5, (255, 0, 255))
+for point in points:
+    x, y, z, color = point
+    zb.set_point(x, y, z, color)
 
-zb.save('z-buffer.png')
+zb.save('ex2.png')
