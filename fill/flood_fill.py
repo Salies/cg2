05d09dd5c5bfead28e0293ac1de495d17a85e247
Salies <serezane@protonmail.com>
2023-01-13 14:55:26 -0300

    flood-fill: comentários

--- fill/flood_fill.py
+++ fill/flood_fill.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+# Vizinhança-4
 def flood_fill(img, xo, yo, paint_color, target_color):
     if paint_color == target_color:
         return
@@ -9,6 +10,9 @@ def flood_fill(img, xo, yo, paint_color, target_color):
     stack = [(xo, yo)]
     while stack:
         x, y = stack.pop()
+        # np.array_equal é uma instrução que compara dois arrays, no caso, duas cores
+        # de resto, é um algoritmo comum de flood fill por pilha (Python é péssimo para recursão).
+        # O mesmo vale para a versão com vizinhança-8.
         if x > 0 and np.array_equal(img[x-1, y], target_color):
             img[x-1, y] = paint_color
             stack.append((x-1, y))
@@ -22,6 +26,7 @@ def flood_fill(img, xo, yo, paint_color, target_color):
             img[x, y+1] = paint_color
             stack.append((x, y+1))
 
+# Vizinhança-8
 def flood_fill_eight(img, xo, yo, paint_color, target_color):
     if paint_color == target_color:
         return
